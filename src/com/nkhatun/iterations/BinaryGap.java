e2e6181325f63f5710325884d07673329c4016af
nkhatun <nknajada@gmail.com>
2022-04-14 10:08:16 +0200

    log info added

--- src/com/nkhatun/iterations/BinaryGap.java
+++ src/com/nkhatun/iterations/BinaryGap.java
@@ -1,11 +1,15 @@
 package com.nkhatun.iterations;
+
+import java.util.logging.Logger;
+
 /*A binary gap within a positive integer N is any maximal sequence of consecutive zeros that is surrounded by ones at both ends in the binary representation of N.
 Write a function:
 class Solution { public int solution(int N); }
 that, given a positive integer N, returns the length of its longest binary gap. The function should return 0 if N doesn't contain a binary gap.*/
 public class BinaryGap {
+	private static Logger log = Logger.getLogger(BinaryGap.class.getName());
 	public static void main(String args[]) {
-		System.out.println(solution(32));
+		log.info("BinaryGap: "+solution(32));
 	}
 
 	public static int solution(int N) {
